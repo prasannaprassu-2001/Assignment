71dbccb89db346a5147252356a5bec48e528dd92
prasannaprassu-2001 <amuprassu11@gmail.com>
2025-03-01 14:51:45 +0530

    UI/UX Assignment

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function bookDemo() {
+    alert("Thank you for your interest! A representative will contact you soon.");
+}
+
+
+function seeAllFeatures() {
+    alert("Redirecting to full features list...");
+}
